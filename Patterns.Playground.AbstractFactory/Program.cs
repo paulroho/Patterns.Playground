f40f1a1bcc0c4c92d0f159041e97bf462b4af23f
Paul Rohorzka <paul@paulroho.com>
2015-06-05 14:04:56 +0200

    Skeleton (not compiling)

---
+++ Patterns.Playground.AbstractFactory/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Patterns.Playground.AbstractFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var supplier = args[0];
+
+            var incredientProvider = GetIncredientProvider(supplier);
+            var factory = new PizzaBaker(incredientProvider);
+
+            var pizzaTomato = factory.CreateTomatoPizza();
+
+            foreach (var incredient in pizzaTomato.Incredients)
+            {
+                Console.WriteLine(incredient);
+            }
+        }
+    }
+}
